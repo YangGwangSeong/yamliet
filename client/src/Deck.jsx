b95d22484931c7b80ae2cd0452004dcf2aa8b2a5
yanggwangseong <rhkdtjd_12@naver.com>
2021-09-06 15:29:37 +0900

    덱에 이미지 사진으로 수정하기.

--- client/src/Deck.jsx
+++ client/src/Deck.jsx
@@ -5,12 +5,12 @@ import { useDrag } from 'react-use-gesture'
 
 
 const cards = [
-  'https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg',
-  'https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg',
-  'https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg',
-  'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
-  'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-  'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg'
+  'https://github.com/YangGwangSeong/yamliet/blob/main/client/src/img/img_1.jpg?raw=true',
+  'https://github.com/YangGwangSeong/yamliet/blob/main/client/src/img/img2.jpg?raw=true',
+  'https://github.com/YangGwangSeong/yamliet/blob/main/client/src/img/img6.jpg?raw=true',
+  'https://github.com/YangGwangSeong/yamliet/blob/main/client/src/img/img7.jpg?raw=true',
+  'https://github.com/YangGwangSeong/yamliet/blob/main/client/src/img/img8.png?raw=true',
+  'https://github.com/YangGwangSeong/yamliet/blob/main/client/src/img/img9.jpg?raw=true'
 ]
 
 // These two are just helpers, they curate spring data, values that are later being interpolated into css
