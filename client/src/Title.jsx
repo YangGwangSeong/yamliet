3d132f36fe0d2ea3ae0d80ecc3efde8b78b26285
yanggwangseong <rhkdtjd_12@naver.com>
2021-09-06 13:20:54 +0900

    타이틀 수정 테스트

--- client/src/Title.jsx
+++ client/src/Title.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function Title(){
 
     return(
-        <div className="title text-center">Hi yamliet!.</div>
+        <div className="title text-center">Hi yamliet!!.</div>
     )
 }
 
